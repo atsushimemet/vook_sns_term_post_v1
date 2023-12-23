fdaf06232318f4d8ec347bf89887e78a454b3aa9
atsushimemet <noap3b69n@gmail.com>
2023-12-24 00:13:33 +0900

    add: done check runned src

--- openai-test.py
+++ openai-test.py
@@ -1,19 +1,30 @@
 from openai import OpenAI
 
+from vook_sns_term_post_v1.config.config import *  # noqa
+
 client = OpenAI()
+
+brand = "Levi's"
+item = "Denim Pants"
+term = "赤耳"
+str_limit = 175
+
+
+role_content = f"あなたは{brand}のヴィンテージに精通している専門家です。"
+prompt = f"{brand}のf{item}に関連する{term}という用語について、{str_limit}文字以内かつ日本語で、用語説明テキストを生成してください。生成した文章が{str_limit}を超えている場合は、生成し直してください。"
 
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {
             "role": "system",
-            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+            "content": role_content,
         },
         {
             "role": "user",
-            "content": "Compose a poem that explains the concept of recursion in programming.",
+            "content": prompt,
         },
     ],
 )
-
-print(completion.choices[0].message)
+content = completion.choices[0].message.content
+print(f"以下のテキストが出力されました。文字数は{len(content)}でした。\n{content}")
